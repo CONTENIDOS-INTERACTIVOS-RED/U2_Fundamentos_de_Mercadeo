11883a2e59ea5b45407da3faf871e1fff6f0b878
flizcanor <flizcanor@icloud.com>
2025-01-12 12:44:23 -0500

    Update Sintesis.vue

--- src/views/curso/Sintesis.vue
+++ src/views/curso/Sintesis.vue
@@ -2,7 +2,7 @@
   .curso-main-container.creditos-vista
     BannerInterno(subTitulo="SÍNTESIS")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-      p(data-aos="fade-up").mb-5 La #[b unidad 1 "Introducción al mercadeo"], establece los fundamentos esenciales para comprender el concepto, las funciones y la evolución del mercadeo en un entorno globalizado. Esta unidad proporciona una perspectiva clara sobre la creación de valor, la satisfacción del cliente y la construcción de relaciones sostenibles entre marcas y consumidores. Además, explora las diferencias entre el marketing tradicional y digital, destacando la importancia de estrategias personalizadas e innovadoras. Este enfoque integral asegura que los estudiantes adquieran una comprensión sólida del mercadeo, permitiéndoles avanzar con éxito, en su análisis y aplicación en diversos contextos empresariales.
+      p(data-aos="fade-up").mb-5 La #[b unidad 2 "Comportamiento del consumidor y segmentación de mercados"], aborda los factores psicológicos, sociales, culturales y personales que influyen en las decisiones de compra, explorando cómo las empresas pueden segmentar mercados utilizando métodos avanzados como el análisis predictivo y la inteligencia artificial. Los estudiantes aprenderán a aplicar estrategias de segmentación basadas en datos demográficos, psicográficos y conductuales, para desarrollar campañas personalizadas y efectivas. Ejemplos de marcas líderes como Apple y Samsung, ilustran cómo estas estrategias optimizan recursos y fortalecen la conexión con los consumidores, destacando la relevancia de estas herramientas en el marketing contemporáneo.
       .row.justify-content-center
         .col-lg-12.mb-5
           figure.bg-color-sintesis.p-5.brounded
