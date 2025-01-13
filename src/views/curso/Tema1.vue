b97ae3587542f05b44ee0465cce71f5023b74506
flizcanor <flizcanor@icloud.com>
2025-01-13 05:19:27 -0500

    Update Tema1.vue

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -9,7 +9,7 @@
       .p-4.p-md-5
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/k51it0apXws?si=PRTsR6rDUjr9hN-l" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
   
 
